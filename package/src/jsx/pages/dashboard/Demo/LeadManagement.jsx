1328c8683729d39da067b1f83f51d751a3fe2720
justtravneet <singhji6868@.com>
2024-10-15 01:41:16 -0700

    first commit

---
+++ package/src/jsx/pages/dashboard/Demo/LeadManagement.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PageTitle from '../../../layouts/PageTitle';
+import Studentdetails from '../../holidays/Studentdetails';
+
+
+
+const LeadManagement= () => {
+    return (
+        <>
+           
+            <Studentdetails />
+        </>
+    );
+};
+
+export default LeadManagement;
