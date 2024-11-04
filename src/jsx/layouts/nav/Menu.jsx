090f0d4c164cdb178b3d973e546a9794bf73f47c
khushalhiranii <khushal.hirani4@gmail.com>
2024-11-04 18:03:52 +0530

    side bar and dashboard

--- src/jsx/layouts/nav/Menu.jsx
+++ src/jsx/layouts/nav/Menu.jsx
@@ -24,102 +24,102 @@ export const MenuList = [
     
 
     //Professors    
-    {
-        title: 'Professors',	
-        classsChange: 'mm-collapse',		
-        iconStyle: <i className="la la-user" />,
-        content: [
-            {
-                title: 'All Professor',
-                to: 'all-professors',					
-            },
-            {
-                title: 'Add Professor',
-                to: 'add-professor',
-            },
-            {
-                title: 'Edit Professor',
-                to: 'edit-professor',
-            },
-            {
-                title: 'Professor Profile',
-                to: 'professor-profile',
-            },
+    // {
+    //     title: 'Professors',	
+    //     classsChange: 'mm-collapse',		
+    //     iconStyle: <i className="la la-user" />,
+    //     content: [
+    //         {
+    //             title: 'All Professor',
+    //             to: 'all-professors',					
+    //         },
+    //         {
+    //             title: 'Add Professor',
+    //             to: 'add-professor',
+    //         },
+    //         {
+    //             title: 'Edit Professor',
+    //             to: 'edit-professor',
+    //         },
+    //         {
+    //             title: 'Professor Profile',
+    //             to: 'professor-profile',
+    //         },
            
-        ],
-    },
-    //Student    
-    {
-        title: 'Students',	
-        classsChange: 'mm-collapse',		
-        iconStyle: <i className="la la-users" />,
-        content: [
-            {
-                title: 'All Students',
-                to: 'all-students',					
-            },
-            {
-                title: 'Add Students',
-                to: 'add-student',
-            },
-            {
-                title: 'Edit Students',
-                to: 'edit-student',
-            },
-            {
-                title: 'About Students',
-                to: 'about-student',
-            },
+    //     ],
+    // },
+    // //Student    
+    // {
+    //     title: 'Students',	
+    //     classsChange: 'mm-collapse',		
+    //     iconStyle: <i className="la la-users" />,
+    //     content: [
+    //         {
+    //             title: 'All Students',
+    //             to: 'all-students',					
+    //         },
+    //         {
+    //             title: 'Add Students',
+    //             to: 'add-student',
+    //         },
+    //         {
+    //             title: 'Edit Students',
+    //             to: 'edit-student',
+    //         },
+    //         {
+    //             title: 'About Students',
+    //             to: 'about-student',
+    //         },
            
-        ],
-    },
+    //     ],
+    // },
 
-    //Courses    
-    {
-        title: 'Courses',	
-        classsChange: 'mm-collapse',		
-        iconStyle: <i className="la la-graduation-cap" />,
-        content: [
-            {
-                title: 'All Courses',
-                to: 'all-courses',					
-            },
-            {
-                title: 'Add Courses',
-                to: 'add-courses',
-            },
-            {
-                title: 'Edit Courses',
-                to: 'edit-courses',
-            },
-            {
-                title: 'About Courses',
-                to: 'about-courses',
-            },
+    // //Courses    
+    // {
+    //     title: 'Courses',	
+    //     classsChange: 'mm-collapse',		
+    //     iconStyle: <i className="la la-graduation-cap" />,
+    //     content: [
+    //         {
+    //             title: 'All Courses',
+    //             to: 'all-courses',					
+    //         },
+    //         {
+    //             title: 'Add Courses',
+    //             to: 'add-courses',
+    //         },
+    //         {
+    //             title: 'Edit Courses',
+    //             to: 'edit-courses',
+    //         },
+    //         {
+    //             title: 'About Courses',
+    //             to: 'about-courses',
+    //         },
            
-        ],
-    },
-    //Library    
-    {
-        title: 'Library',	
-        classsChange: 'mm-collapse',		
-        iconStyle: <i className="la la-book" />,
-        content: [
-            {
-                title: 'All Library',
-                to: 'all-library',					
-            },
-            {
-                title: 'Add Library',
-                to: 'add-library',
-            },
-            {
-                title: 'Edit Library',
-                to: 'edit-library',
-            },            
+    //     ],
+    // },
+    // //Library    
+    // {
+    //     title: 'Library',	
+    //     classsChange: 'mm-collapse',		
+    //     iconStyle: <i className="la la-book" />,
+    //     content: [
+    //         {
+    //             title: 'All Library',
+    //             to: 'all-library',					
+    //         },
+    //         {
+    //             title: 'Add Library',
+    //             to: 'add-library',
+    //         },
+    //         {
+    //             title: 'Edit Library',
+    //             to: 'edit-library',
+    //         },            
            
-        ],
-    },
+    //     ],
+    // },
     //   Lead
     {
         title: 'Lead',	        
@@ -130,54 +130,54 @@ export const MenuList = [
 
 
     
-    {
-        title: 'CMS',	
-        classsChange: 'mm-collapse',		
-        iconStyle: <i className="la la-book" />,
-        content: [
-            {
-                title: 'Content',
-                to: 'content',					
-            },
-            {
-                title: 'Menu',
-                to: 'menu',
-            },
-            {
-                title: 'Email-template',
-                to: 'email-template',
-            }, 
-            {
-                title: 'Blog',
-                to: 'blog',
-            },            
-            {
-                title: 'Content-add',
-                to: 'content-add',
-            }, 
-            {
-                title: 'Add-email',
-                to: 'add-email',
-            }, 
+    // {
+    //     title: 'CMS',	
+    //     classsChange: 'mm-collapse',		
+    //     iconStyle: <i className="la la-book" />,
+    //     content: [
+    //         {
+    //             title: 'Content',
+    //             to: 'content',					
+    //         },
+    //         {
+    //             title: 'Menu',
+    //             to: 'menu',
+    //         },
+    //         {
+    //             title: 'Email-template',
+    //             to: 'email-template',
+    //         }, 
+    //         {
+    //             title: 'Blog',
+    //             to: 'blog',
+    //         },            
+    //         {
+    //             title: 'Content-add',
+    //             to: 'content-add',
+    //         }, 
+    //         {
+    //             title: 'Add-email',
+    //             to: 'add-email',
+    //         }, 
            
-            {
-                title: 'Add-blog',
-                to: 'add-blog',
-            }, 
+    //         {
+    //             title: 'Add-blog',
+    //             to: 'add-blog',
+    //         }, 
 
-            {
-                title: 'Blog-category',
-                to: 'blog-category',
-            }, 
+    //         {
+    //             title: 'Blog-category',
+    //             to: 'blog-category',
+    //         }, 
 
-            {
-                title: 'Add-email',
-                to: 'add-email',
-            }, 
+    //         {
+    //             title: 'Add-email',
+    //             to: 'add-email',
+    //         }, 
 
            
-        ],
-    },
+    //     ],
+    // },
     
    
 
